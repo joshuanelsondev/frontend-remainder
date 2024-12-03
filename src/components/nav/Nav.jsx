44ac5aec6f9bcec4d3a7fee499192dbc22b38394
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-03 15:12:22 -0500

    refactor: style nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,17 +9,25 @@ export default function Nav() {
         Remainder
       </Link>
       <ul className="center-links">
-        <li className="center-links__link">
-          <Link to={"/about"}>About</Link>
+        <li>
+          <Link className="center-links__link" to={"/about"}>
+            About
+          </Link>
         </li>
-        <li className="center-links__link">
-          <Link to={"/features"}>Features</Link>
+        <li>
+          <Link className="center-links__link" to={"/features"}>
+            Features
+          </Link>
         </li>
-        <li className="center-links__link">
-          <Link to={"/plansAndPricing"}>Plans & Pricing</Link>
+        <li>
+          <Link className="center-links__link" to={"/plansAndPricing"}>
+            Plans & Pricing
+          </Link>
         </li>
-        <li className="center-links__link">
-          <Link to={"/contact"}>Contact</Link>
+        <li>
+          <Link className="center-links__link" to={"/contact"}>
+            Contact
+          </Link>
         </li>
       </ul>
       <div className="right-links">
