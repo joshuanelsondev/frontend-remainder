225342233dfa00318a5f2071f740823a97fb260b
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-07 18:40:39 -0500

    refactor: make updates for handling MFA for registering a user

---
+++ src/pages/mfa/MFASetupPage.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { client } from "@passwordless-id/webauthn";
+import { getRegistrationOptions } from "../../api/auth";
+import { useModal } from "../../context/ModalContext";
+import "./MFASetupPage.scss";
+
+export default function MFASetupPage() {
+  const [searchParams] = useSearchParams();
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const email = searchParams.get("email");
+  const { setActiveModal } = useModal();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const setupMFA = async () => {
+      try {
+        if (!email) {
+          throw new Error("Email is required to complete MFA setup.");
+        }
+        console.log("EMAIL:", email);
+
+        const registrationOptions = await getRegistrationOptions(email);
+
+        console.log("REGISTRATIONOPTIONS:", registrationOptions);
+        await client.register({
+          challenge: registrationOptions.challenge,
+          user: registrationOptions.user,
+          ...registrationOptions,
+        });
+
+        alert("MFA setup completed successfully!");
+        navigate("/");
+        setActiveModal("login");
+      } catch (error) {
+        console.error("MFA setup error:", error);
+
+        setError(
+          error.response?.data?.message || error.message || "MFA setup failed"
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    setupMFA();
+  }, [email, navigate]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>MFA Setup</h1>
+        <p>Setting up your MFA. Please wait...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>MFA Setup</h1>
+        <p style={{ color: "red" }}>{error}</p>
+        <button onClick={() => window.location.reload()}>Retry</button>
+      </div>
+    );
+  }
+
+  return null;
+}
