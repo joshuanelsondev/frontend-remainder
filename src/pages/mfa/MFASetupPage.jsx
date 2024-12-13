cad8317e465db62174b38da41e534f1c7f5dee01
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-13 02:55:49 -0500

    refactor: update mfa logic

--- src/pages/mfa/MFASetupPage.jsx
+++ src/pages/mfa/MFASetupPage.jsx
@@ -1,65 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useContext } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { registerUser } from "../../api/auth";
-import { useModal } from "../../context/ModalContext";
 import "./MFASetupPage.scss";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function MFASetupPage() {
   const [searchParams] = useSearchParams();
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
   const email = searchParams.get("email");
   const token = searchParams.get("token");
-  const { setActiveModal } = useModal();
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (token) {
-      sessionStorage.setItem("authToken", token);
-    }
-    const setupMFA = async () => {
-      try {
-        if (!email) {
-          throw new Error("Email is required to complete MFA setup.");
-        }
-
-        await registerUser(email);
-
-        alert("MFA setup completed successfully!");
-        // navigate("/");
-        // setActiveModal("login");
-      } catch (error) {
-        console.error("MFA setup error:", error);
-
-        setError(
-          error.response?.data?.message || error.message || "MFA setup failed"
-        );
-      } finally {
-        setIsLoading(false);
+  const setupMFA = async () => {
+    try {
+      if (!email) {
+        throw new Error("Email is required to complete MFA setup.");
       }
-    };
 
-    setupMFA();
-  }, [email, token, navigate]);
+      await registerUser(email);
 
-  if (isLoading) {
-    return (
-      <div>
-        <h1>MFA Setup</h1>
-        <p>Setting up your MFA. Please wait...</p>
+      login(token);
+      navigate("/");
+    } catch (error) {
+      console.error("MFA setup error:", error);
+
+      setError(
+        error.response?.data?.message || error.message || "MFA setup failed"
+      );
+    }
+  };
+
+  return (
+    <div className="mfa">
+      <div className="mfa__overlay"></div>
+      <div className="mfa__modal">
+        <h1 className="mfa__header">Mult-factor Authentication</h1>
+        {!error ? (
+          <>
+            <p className="mfa__text">
+              Click below to set up authentication options
+            </p>
+            <button onClick={setupMFA} className="mfa__btn">
+              Start
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="mfa__error">{error}</p>
+            <button className="mfa__btn" onClick={() => setError(null)}>
+              Retry
+            </button>
+          </>
+        )}
       </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div>
-        <h1>MFA Setup</h1>
-        <p style={{ color: "red" }}>{error}</p>
-        <button onClick={() => window.location.reload()}>Retry</button>
-      </div>
-    );
-  }
-
-  return null;
+    </div>
+  );
 }
