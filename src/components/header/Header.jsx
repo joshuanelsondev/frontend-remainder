be9a94452185fb9a302ad054c0324da306aebaff
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-15 23:26:04 -0500

    refactor: create capitalizeStr util and rename instances of Balance/balance to Budget/budget

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLocation } from "react-router";
 import "./Header.scss";
 import { FaUserCircle, FaSortDown } from "react-icons/fa";
+import { AuthContext } from "../../context/AuthContext";
+import capitalizeStr from "../../utils/capitalizeStr";
 
 const routes = {
   "/": "Dashboard",
@@ -14,16 +16,27 @@ const routes = {
 
 export default function Header() {
   const location = useLocation();
+  const { user, isLoggedIn } = useContext(AuthContext);
+  console.log("TEST:", user, isLoggedIn);
 
   return (
     <div className="header">
       <h4 className="header__page">{routes[location.pathname]}</h4>
       <div className="header-right">
         <FaUserCircle size={24} />
-        <div className="header-right__user">
-          <p className="header-right__name">User Name</p>
-          <p className="header-right__email">useremail@mail.com</p>
-        </div>
+        {isLoggedIn && user ? (
+          <div className="header-right__user">
+            <p className="header-right__name">
+              {capitalizeStr(`${user.firstName} ${user.lastName}`)}
+            </p>
+            <p className="header-right__email">{user.email}</p>
+          </div>
+        ) : (
+          <div className="header-right__user">
+            <p className="header-right__name">Guest</p>
+            <p className="header-right__email">guest@mail.com</p>
+          </div>
+        )}
         <FaSortDown />
       </div>
     </div>
