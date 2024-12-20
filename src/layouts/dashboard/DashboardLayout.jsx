e5c44342d659f0d701131ccc35457db6b81d6aab
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-19 20:26:50 -0500

    refactor: wrap DashboardLayout within UserDataContext provider

--- src/layouts/dashboard/DashboardLayout.jsx
+++ src/layouts/dashboard/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import SideNav from "../../components/sideNav/SideNav";
 import Header from "../../components/header/Header";
 import { AuthContext } from "../../context/AuthContext";
-//import { UserDataProvider } from "../../context/UserDataContext";
+import { UserDataProvider } from "../../context/UserDataContext";
 import "./DashboardLayout.scss";
 
 export default function DashboardLayout() {
@@ -14,12 +14,14 @@ export default function DashboardLayout() {
   }
 
   return (
-    <div className="dashboard-layout">
-      <SideNav />
-      <div className="outlet">
-        <Header />
-        <Outlet />
+    <UserDataProvider>
+      <div className="dashboard-layout">
+        <SideNav />
+        <div className="outlet">
+          <Header />
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </UserDataProvider>
   );
 }
