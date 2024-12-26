037498bb13352008e35631203fd7c7a016fbd362
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-25 23:30:15 -0500

    refactor: handle error styling

--- src/modals/login/LoginModal.jsx
+++ src/modals/login/LoginModal.jsx
@@ -2,17 +2,25 @@ import React, { useState, useRef, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import axios from "../../api/axios";
+import validateInput from "../../utils/validateInput";
+import { loginUser } from "../../api/auth";
 import { authenticateUser } from "../../api/auth";
 import useClickOutside from "../../hooks/useClickOutside";
 import { AuthContext } from "../../context/AuthContext";
+import { FaRegEyeSlash, FaRegEye, FaInfoCircle } from "react-icons/fa";
 import "./LoginModal.scss";
 
 export default function LoginModal({ setActiveModal }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState(null);
+  const [pwdVisibility, setPwdVisibility] = useState(false);
+  const [pwdInfo, setPwdInfo] = useState(false);
   const [successModal, setSuccessModal] = useState(null);
   const [useWebAuthn, setUseWebAuthn] = useState(false);
+  const [errors, setErrors] = useState({});
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const formRef = useRef(null);
@@ -21,23 +29,39 @@ export default function LoginModal({ setActiveModal }) {
 
   const handleLogin = async (event) => {
     event.preventDefault();
-    try {
-      const response = await axios.post("/auth/login", { email, password });
-      const { token } = response.data;
 
-      if (!token) {
-        throw new Error("invalid response: token is missing.");
-      }
+    const allErrors = {};
+
+    Object.keys(formState).forEach((fieldName) => {
+      const fieldErrors = validateInput(fieldName, formState[fieldName]);
+      Object.assign(allErrors, fieldErrors);
+    });
+
+    if (Object.keys(allErrors).length > 0) {
+      setErrors(allErrors);
+      return;
+    }
+
+    try {
+      const { email, password } = formState;
+      const response = await loginUser(email, password);
+
+      console.log("Login response:", response);
+      //const { token } = response.data;
+
+      //if (!token) {
+      //  throw new Error("invalid response: token is missing.");
+      //}
 
       login(token);
-      setSuccessModal(true);
+      setMessage("Successful Login");
       setTimeout(() => {
         setActiveModal(null);
       }, 1200);
-      navigate("/dashboard");
+      //navigate("/");
     } catch (error) {
       console.error("Login failed", error);
-      setError(
+      setMessage(
         error.response?.data?.message || "Login failed. Please try again."
       );
     }
@@ -50,24 +74,19 @@ export default function LoginModal({ setActiveModal }) {
       setSuccessModal(true);
       setTimeout(() => {
         setActiveModal(null);
-        navigate("/dashboard");
+        navigate("/");
       }, 1200);
     } catch (error) {
       console.error("Authentication failed:", error);
-      setError(error.message || "Authentication failed. Please try again.");
+      setMessage(error.message || "Authentication failed. Please try again.");
     }
   };
 
   const handleFormInput = (e) => {
-    const { id, value } = e.target;
-    if (id == "email") {
-      setEmail(value);
-    } else {
-      setPassword(value);
-    }
-    if (error) {
-      setError(null);
-    }
+    const { name, value } = e.target;
+    setFormState({ ...formState, [name]: value });
+    setErrors({});
+    setMessage(null);
   };
 
   return (
@@ -92,67 +111,94 @@ export default function LoginModal({ setActiveModal }) {
           Sign in with Passkey
         </button>
       </div> */}
-      {!successModal ? (
-        <form ref={formRef} className="login__form" onSubmit={handleLogin}>
-          <h2 className="login__header">Login</h2>
-          <div className="login__email">
-            <input
-              id="email"
-              value={email}
-              onChange={(e) => handleFormInput(e)}
-              placeholder=""
-              className="login__input"
-              type="email"
-              required
-            />
-            <label className="login__label" htmlFor="email">
-              Email
-            </label>
-          </div>
-          <div className="login__password">
-            <input
-              id="password"
-              value={password}
-              onChange={(e) => handleFormInput(e)}
-              placeholder=""
-              className="login__input"
-              type="password"
-              required
-            />
-            <label className="login__label" htmlFor="password">
-              Password
-            </label>
-          </div>
-          <button type="submit" className="login__sign-in">
-            Sign in
-          </button>
-          <div className="login__links">
-            <button
-              type="button"
-              onClick={() => setActiveModal("resetPassword")}
-              className="login__resetPassword"
-            >
-              Forgot Password?
-            </button>
-            <button
-              type="button"
-              onClick={() => setActiveModal("signup")}
-              className="login__signup"
-            >
-              Create account
-            </button>
-          </div>
-          {error && (
-            <p className="login__error" aria-live="polite">
-              {error}
-            </p>
-          )}
-        </form>
-      ) : (
-        <div className="success">
-          <p className="success__message">Successful Login</p>
+      <form
+        ref={formRef}
+        className="login__form"
+        onSubmit={handleLogin}
+        noValidate
+      >
+        <h2 className="login__header">Login</h2>
+        <div className="login__email">
+          <input
+            id="email"
+            name="email"
+            value={formState.email}
+            onChange={(e) => handleFormInput(e)}
+            placeholder=""
+            className="login__input"
+            type="email"
+            required
+          />
+          <label className="login__label" htmlFor="email">
+            Email
+          </label>
+          {errors.email && <p className="login__error">{errors.email}</p>}
         </div>
-      )}
+        <div className="login__password">
+          <input
+            id="password"
+            name="password"
+            value={formState.password}
+            onChange={(e) => handleFormInput(e)}
+            placeholder=""
+            className="login__input"
+            type={`${pwdVisibility ? "text" : "password"}`}
+            required
+          />
+          <label className="login__label" htmlFor="password">
+            Password
+          </label>
+          {!pwdVisibility ? (
+            <FaRegEyeSlash
+              onClick={() => setPwdVisibility(!pwdVisibility)}
+              className="login__pwd-visibility"
+            />
+          ) : (
+            <FaRegEye
+              onClick={() => setPwdVisibility(!pwdVisibility)}
+              className="login__pwd-visibility"
+            />
+          )}
+          <div className="login__pwd-info-icon">
+            <FaInfoCircle
+              onClick={() => setPwdInfo(!pwdInfo)}
+              title="Password Requirements"
+            />
+            {pwdInfo && (
+              <p className="login__pwd-info-text">
+                Password must be at least 10 characters long, include an
+                uppercase letter, a lowercase letter, a number, and a special
+                character.
+              </p>
+            )}
+          </div>
+          {errors.password && <p className="login__error">{errors.password}</p>}
+        </div>
+        <button type="submit" className="login__sign-in">
+          Sign in
+        </button>
+        <div className="login__links">
+          <button
+            type="button"
+            onClick={() => setActiveModal("resetPassword")}
+            className="login__resetPassword"
+          >
+            Forgot Password?
+          </button>
+          <button
+            type="button"
+            onClick={() => setActiveModal("signup")}
+            className="login__signup"
+          >
+            Create account
+          </button>
+        </div>
+        {message && (
+          <p className="login__message" aria-live="polite">
+            {message}
+          </p>
+        )}
+      </form>
     </div>
   );
 }
