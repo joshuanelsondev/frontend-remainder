47786a486951d3687adead6b17dac7dd235a0f1f
joshuanelsondev <joshuanelsondev@gmail.com>
2025-01-01 19:19:07 -0500

    refactor: update logic to handle year selection and caching

--- src/context/UserDataContext.jsx
+++ src/context/UserDataContext.jsx
@@ -16,6 +16,7 @@ const UserDataContext = createContext();
 
 export const useUserData = () => useContext(UserDataContext);
 
+// Provider Function
 export const UserDataProvider = ({ children }) => {
   const currentYear = new Date().getFullYear();
   const [selectedYear, setSelectedYear] = useState(currentYear);
@@ -25,20 +26,29 @@ export const UserDataProvider = ({ children }) => {
     expenses: [],
     budget: { totalIncome: 0, totalExpenses: 0, disposableIncome: 0 },
   });
+  const [yearlyComparisons, setYearlyComparisons] = useState({});
 
+  // Make API calls to retrieve user data
   const getUserData = throttle(async (year) => {
     try {
       const startDate = `${year}-01-01`;
       const endDate = `${year}-12-31`;
 
-      const [incomes, expenses, budget, comparisons] = await Promise.all([
-        getAllIncomes(),
-        getAllExpenses(),
-        getBudget(),
-        getComparisons(startDate, endDate),
-      ]);
+      if (!yearlyComparisons[year]) {
+        const [incomes, expenses, budget, comparisons] = await Promise.all([
+          getAllIncomes(),
+          getAllExpenses(),
+          getBudget(),
+          getComparisons(startDate, endDate),
+        ]);
 
-      setUserData({ incomes, expenses, budget, comparisons });
+        setUserData({ incomes, expenses, budget, comparisons });
+
+        // Cache comparisons for the year
+        setYearlyComparisons({ ...prev, [year]: comparisons });
+      } else {
+        setUserData({ ...prev, comparisons: yearlyComparisons[year] });
+      }
     } catch (error) {
       console.error("Error fetching user data:", error);
     } finally {
@@ -60,6 +70,7 @@ export const UserDataProvider = ({ children }) => {
         selectedYear,
         setSelectedYear,
         isLoading,
+        yearlyComparisons,
       }}
     >
       {children}
