e4e3186911b8fae0407ec3438603568c0a2e4907
joshuanelsondev <joshuanelsondev@gmail.com>
2025-01-07 11:31:42 -0500

    styling: update styling for RecentActivity widget

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Settings.scss";
+
+const navLinks = [
+  {
+    displayName: "User",
+    identifier: "user",
+  },
+  {
+    displayName: "Password",
+    identifier: "password",
+  },
+  {
+    displayName: "Notifications",
+    identifier: "notification",
+  },
+  {
+    displayName: "Additional Settings",
+    identifier: "additional",
+  },
+];
 
 export default function Settings() {
+  const [activeLink, setActiveLink] = useState("user");
+
   return (
-    <div>
-      <h4>Settings</h4>
+    <div className="settings">
+      <ul className="settings__nav">
+        {navLinks.map((link, i) => (
+          <li
+            key={i}
+            onClick={() => setActiveLink(link.identifier)}
+            className={`settings__nav-link${
+              activeLink === link.identifier ? "-active" : ""
+            }`}
+          >
+            {link.displayName}
+          </li>
+        ))}
+      </ul>
+      <div className="settings__forms"></div>
     </div>
   );
 }
